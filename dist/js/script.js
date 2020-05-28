3a7c29f79b1fabfdb270683ee7bbe9e5485159b9
MedvedievM <medvediev.m91@gmail.com>
2020-05-28 23:47:39 +0300

    add modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,5 +93,26 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('.timer', deadLine);
+
+
+    // Modal 
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });      
+  
+    modalCloseBtn.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }); 
     
 });
